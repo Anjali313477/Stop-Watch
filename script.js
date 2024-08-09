95dcfdef3d19087d847847e960eb15b8afe7f280
anjali313477 <anjalitadiyal5@outlook.com>
2024-08-09 11:50:52 +0530

    stop-watch

---
+++ script.js
@@ -0,0 +1,39 @@
+let time = document.querySelector('.time');
+let startbutton = document.querySelector("#startbtn");
+let stopbutton = document.querySelector("#stopbtn");
+let resetbutton = document.querySelector("#resetbtn")
+let hrs = 0;
+let min = 0;
+let sec = 0;
+let ID;
+
+
+startbutton.addEventListener('click', (event) => {
+  ID = setInterval(start, 1000)
+})
+
+stopbutton.addEventListener('click', (event) => {
+  clearInterval(ID)
+});
+
+resetbutton.addEventListener('click', (event) => {
+  clearInterval(ID);
+  time.innerHTML = "00:00:00"
+  hrs = 0;
+  min = 0;
+  sec = 0;
+})
+
+function start() {
+  sec++;
+  if (sec == 60) {
+    sec = 0;
+    min++;
+    if (min == 60) {
+      min = 0;
+      hrs++
+    }
+  }
+  document.querySelector('.time').innerHTML = `${hrs} : ${min}: ${sec}`
+
+}
